406fec3662fb8d01532209c74598e46aa12d6beb
K Annabel Bligh <kabligh@gmail.com>
2020-12-05 16:41:18 +0000

    Added basic function to calculate score

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -9,4 +9,18 @@ describe("Frame", function() {
   it('has a starting score of zero', function() {
     expect(frame.getScore()).toEqual(0);
   });
+
+  it('starts with an empty array of rolls', function() {
+    expect(frame.rolls()).toEqual([]);
+  });
+
+  it('lets the user knock 10 or less pins per roll', function() {
+    frame.knock(10);
+    expect(frame.rolls()).toEqual([10]);
+  });
+
+  it('adds the pins knocked over to the score', function() {
+    frame.knock(10);
+    expect(frame.getScore()).toEqual(10);
+  });
 });
--- src/frame.js
+++ src/frame.js
@@ -4,9 +4,19 @@ class Frame{
 
   constructor() {
     this.score = 0
+    this._rolls = []
   }
 
   getScore() {
     return this.score;
+  };
+
+  rolls() {
+    return this._rolls;
+  };
+
+  knock(pins) {
+    this._rolls.push(pins);
+    this.score += pins;
   };
 };
